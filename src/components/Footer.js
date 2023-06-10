c5979a6b75a36c068af5123bb81cad0050bd4f34
mlepe <you@example.com>
2023-06-10 12:14:01 +0400

    Modified footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,14 @@
  *   Copyright (c) 2021 Matthieu Leperlier
  *   All rights reserved.
  */
+import React from "react";
+
 const Footer = () => {
   return (
     <div className="Footer">
       <span>
         Made by Matthieu Leperlier{" "}
-        <a href="https://github.com/mlepe">(@mlepe)</a> (c){" "}
-        {getYear(new Date())}
+        <a href="https://github.com/mlepe">(@mlepe)</a> (c) 2023
       </span>
     </div>
   );
